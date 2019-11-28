a9f940f061179c788f1c54f65e1d1abb16808fce
Yerden Zhumabekov <yerden.zhumabekov@gmail.com>
2019-11-28 16:38:16 +0600

    ci: add ethdev/flow [WIP]
    Signed-off-by: Yerden Zhumabekov <yerden.zhumabekov@gmail.com>

--- .travis.govet.sh
+++ .travis.govet.sh
@@ -7,7 +7,7 @@ export CGO_LDFLAGS_ALLOW=".*"
 export CGO_LDFLAGS="-Wl,--dynamic-linker=/lib64/ld-linux-x86-64.so.2"
 
 set -e
-DIRS="common eal ethdev lcore mempool ring port memzone"
+DIRS="common eal ethdev lcore mempool ring port memzone ethdev/flow"
 # Add subdirectories here as we clean up golint on each.
 for subdir in $DIRS; do
   pushd $subdir
